c15190db274eedefc37c095f5d671de6425b0e34
naveen <kalanaveen55@gmail.com>
2022-03-29 16:56:07 +0530

    arrow function added

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let count = 0;
 let value = document.querySelector("#value");
 let btns = document.querySelectorAll(".btn");
 
-btns.forEach(function(btn){
-    btn.addEventListener("click",function(e){
+btns.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
         const styles = e.target.classList;
         if(styles.contains("decrease")){
             count--;
